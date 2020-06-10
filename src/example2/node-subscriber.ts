4842b63a214e47f81d924e4e7e4d77ebf6a025d6
ros2jsguy <66370745+ros2jsguy@users.noreply.github.com>
2020-06-10 16:01:08 -0500

    Introduces the following examples: * example1 * example2

---
+++ src/example2/node-subscriber.ts
@@ -0,0 +1,28 @@
+// Example-2, Create Node and simple Subscription
+
+'strict mode';
+
+
+import * as rclnodejs from 'rclnodejs';
+
+async function main() {
+
+    // initialize the ros-client-library
+    await rclnodejs.init();
+
+    // create a node and subscription for topic: msg'
+    let node = rclnodejs.createNode('ros2js_examples_node_subscriber'); 
+    let subscriber = node.createSubscription('std_msgs/msg/String', 'msg', undefined, 
+      (msg: rclnodejs.Message) => {
+        console.log('msg: ', (msg as rclnodejs.std_msgs.msg.String).data);
+      });
+
+    console.log('node-subscriber waiting for messages');
+
+    // start accepting msgs
+    rclnodejs.spin(node);
+}
+
+// run the program
+main();
+ 
